1932b01a0f920d7be0052446926edce246cf2f39
jbackstrom <s0462390@student.rockvalleycollege.edu>
2015-09-28 23:43:02 -0500

    jb

--- My Contacts/ViewController.swift
+++ My Contacts/ViewController.swift
@@ -9,15 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var ScrollView: UIScrollView!
+    @IBOutlet weak var txtFirst: UITextField!
+    @IBOutlet weak var txtLast: UITextField!
+    @IBOutlet weak var txtAddress: UITextField!
+    @IBOutlet weak var txtCity: UITextField!
+    @IBOutlet weak var txtState: UITextField!
+    @IBOutlet weak var txtZip: UITextField!
+    @IBOutlet weak var txtPhone: UITextField!
+    @IBOutlet weak var txtEmail: UITextField!
+    @IBOutlet weak var txtContacts: UITextView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    @IBAction func btnBack(sender: UIButton) {
     }
-
+    @IBOutlet weak var btnBack: UIButton!
+   
+    
+    @IBAction func btnView(sender: UIButton) {
+    }
+    
+    @IBAction func btnSave(sender: UIButton) {
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    // 1) Add MessageBox function using AlertView
+    func MsgBox(message:NSString)
+    {
+        //Create Alert
+        var alert = UIAlertView()
+        alert.title = "Alert"
+        alert.message = message as String
+        alert.addButtonWithTitle("OK")
+        alert.show()
+    }
+    
+    
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
     }
 
 
